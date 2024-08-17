b866ac6204b35091b00c831256776f1d056b5c6b
Marshall <hieptt.dev@gmail.com>
2024-08-17 15:58:57 +0700

    finish home and about page

--- website-supply-chain-management/src/App.js
+++ website-supply-chain-management/src/App.js
@@ -1,19 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./layout/header/Header";
 import Footer from "./layout/Footer";
-import Home from "./components/Home";
+import Home from "./components/home/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Header />
-        <Routes>
-            <Route path="/home" element={<Home />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+          <Route path="/" element={<Home/>} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- website-supply-chain-management/src/layout/header/Header.js
+++ website-supply-chain-management/src/layout/header/Header.js
@@ -1,18 +1,47 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Header.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 const Header = () => {
+    const location = useLocation();
+
     return (
-        <Navbar expand="lg" className="navbar__custom">
+        <Navbar expand="lg" className="navbar-custom">
             <Container>
-                <Navbar.Brand as={NavLink} to="/" className="me-auto navbar__custom__logo">F&H Logistic</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Link to="/" className="navbar-brand navbar-custom__logo">F&H Logistic</Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto navbar__custom__menu">
-                        <Nav.Link as={NavLink} to="/home" className="navbar__custom__menu--item" activeClassName="active">Trang chủ</Nav.Link>
-                        <Nav.Link as={NavLink} to="/ordertracking" className="navbar__custom__menu--item" activeClassName="active">Theo dõi đơn hàng</Nav.Link>
-                        <Nav.Link as={NavLink} to="/login" className="navbar__custom__menu--item" activeClassName="active">Đăng nhập</Nav.Link>
+                    <Nav className="ms-auto navbar-custom__menu">
+                        <Link 
+                            to="/" 
+                            className={`nav-link navbar-custom__menu--item ${location.pathname === "/" ? "active" : ""}`}
+                        >
+                            Trang chủ
+                        </Link>
+                        <Link 
+                            to="/about" 
+                            className={`nav-link navbar-custom__menu--item ${location.pathname === "/about" ? "active" : ""}`}
+                        >
+                            Giới thiệu
+                        </Link>
+                        <Link 
+                            to="/order" 
+                            className={`nav-link navbar-custom__menu--item ${location.pathname === "/order" ? "active" : ""}`}
+                        >
+                            Đặt hàng
+                        </Link>
+                        <Link 
+                            to="/tracking" 
+                            className={`nav-link navbar-custom__menu--item ${location.pathname === "/tracking" ? "active" : ""}`}
+                        >
+                            Theo dõi đơn hàng
+                        </Link>
+                        <Link 
+                            to="/login" 
+                            className={`nav-link navbar-custom__menu--item ${location.pathname === "/login" ? "active" : ""}`}
+                        >
+                            Đăng nhập
+                        </Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
