911c37ac06554c058ede7cc14d522164e960a6fb
Marshall <hieptt.dev@gmail.com>
2024-08-26 21:35:48 +0700

    finish product

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import productDefaultImage from '../../images/Product/product_default.jpg';
-import Spinner from 'react-bootstrap/Spinner';
 import APIs, { endpoints } from "../../configs/APIs";
 import './Product.css';
 
@@ -9,15 +8,13 @@ const Product = () => {
     const [page, setPage] = useState(1);
     const [size, setSize] = useState(8);
     const [loading, setLoading] = useState(true);
-    const [q, setQ] = useState('');
 
     const loadProduct = async () => {
         setLoading(true);
         try {
             let params = new URLSearchParams({
                 page: page,
-                size: size,
-                q: q
+                size: size
             }).toString();
 
             let res = await APIs.get(`${endpoints.products}?${params}`);
@@ -31,12 +28,7 @@ const Product = () => {
 
     useEffect(() => {
         loadProduct();
-    }, [page, size, q]);
-
-    const handleSearch = (event) => {
-        event.preventDefault();
-        setQ(event.target.value);
-    };
+    }, [page, size]);
 
     const handleNextPage = () => {
         setPage(prevPage => prevPage + 1);
@@ -48,9 +40,7 @@ const Product = () => {
 
     if (loading) {
         return (
-            <div className='text-center'>
-                <Spinner animation="border" />
-            </div>
+           <h1>Hello</h1>
         );
     }
 
@@ -66,51 +56,51 @@ const Product = () => {
                     type='text'
                     className='product__search--input'
                     placeholder='Tìm kiếm sản phẩm...'
-                    value={q}
-                    onChange={handleSearch}
                 />
             </div>
 
-            <div className='row'>
-                {products.map((product) => (
-                    <div key={product.id} className='col-3 mb-4'>
-                        <div className='card-product'>
-                            <div className='card-product__image'>
-                                <img
-                                    src={product.image ? product.image : productDefaultImage}
-                                    alt={product.name}
-                                />
-                            </div>
+            <div className='product-content'>
+                <div className='row'>
+                    {products.map((product) => (
+                        <div key={product.id} className='col-3 mb-4'>
+                            <div className='card-product'>
+                                <div className='card-product__image'>
+                                    <img
+                                        src={product.image ? product.image : productDefaultImage}
+                                        alt={product.name}
+                                    />
+                                </div>
 
-                            <div className='card-product__content'>
-                                <h1 className='card-product__content--title'>Tên sản phẩm: {product.name}</h1>
-                                <p className='card-product__content--description'>Mô tả: {product.description}</p>
-                                <span className='card-product__content--price'>Giá: {product.price} VNĐ</span>
-                            </div>
+                                <div className='card-product__content'>
+                                    <h1 className='card-product__content--title'>Tên sản phẩm: {product.name}</h1>
+                                    <p className='card-product__content--description'>Mô tả: {product.description}</p>
+                                    <span className='card-product__content--price'>Giá: {product.price} VNĐ</span>
+                                </div>
 
-                            <div className='card-product__button'>
-                                <button className='card-product__button--detail'>Xem chi tiết</button>
-                                <button className='card-product__button--order'>Đặt hàng</button>
+                                <div className='card-product__button'>
+                                    <button className='card-product__button--detail'>Xem chi tiết</button>
+                                    <button className='card-product__button--order'>Đặt hàng</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
 
-            <div className='text-center'>
-                <button
-                    className='btn-page me-2'
-                    onClick={handlePrevPage}
-                    disabled={page === 1}
-                >
-                    <i className='bx bxs-left-arrow' ></i>
-                </button>
-                <button
-                    className='btn-page'
-                    onClick={handleNextPage}
-                >
-                    <i className='bx bxs-right-arrow'></i>
-                </button>
+                <div className='text-center'>
+                    <button
+                        className='btn-page me-2'
+                        onClick={handlePrevPage}
+                        disabled={page === 1}
+                    >
+                        <i className='bx bxs-left-arrow' ></i>
+                    </button>
+                    <button
+                        className='btn-page'
+                        onClick={handleNextPage}
+                    >
+                        <i className='bx bxs-right-arrow'></i>
+                    </button>
+                </div>
             </div>
         </div>
     );
